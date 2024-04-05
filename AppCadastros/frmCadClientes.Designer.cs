7cb305f213168aa3dccd89cb5ef7c7a1d6db7537
alberto-Lucas <100029506+alberto-Lucas@users.noreply.github.com>
2024-04-05 19:01:53 -0300

    Aula dia 02/04/2024

--- AppCadastros/frmCadClientes.Designer.cs
+++ AppCadastros/frmCadClientes.Designer.cs
@@ -33,6 +33,8 @@
             this.txtNomeCompleto = new System.Windows.Forms.TextBox();
             this.btnSalvar = new System.Windows.Forms.Button();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.mskCPF = new System.Windows.Forms.MaskedTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,6 +53,7 @@
             this.txtNomeCompleto.Name = "txtNomeCompleto";
             this.txtNomeCompleto.Size = new System.Drawing.Size(192, 20);
             this.txtNomeCompleto.TabIndex = 1;
+            this.txtNomeCompleto.Validating += new System.ComponentModel.CancelEventHandler(this.txtNomeCompleto_Validating);
             // 
             // btnSalvar
             // 
@@ -61,16 +64,37 @@
             this.btnSalvar.TabIndex = 2;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
             // errorProvider1
             // 
             this.errorProvider1.ContainerControl = this;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CPF";
+            // 
+            // mskCPF
+            // 
+            this.mskCPF.Location = new System.Drawing.Point(238, 25);
+            this.mskCPF.Mask = "999.999.999-99";
+            this.mskCPF.Name = "mskCPF";
+            this.mskCPF.Size = new System.Drawing.Size(84, 20);
+            this.mskCPF.TabIndex = 4;
+            this.mskCPF.Validating += new System.ComponentModel.CancelEventHandler(this.mskCPF_Validating);
             // 
             // frmCadClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(357, 140);
+            this.Controls.Add(this.mskCPF);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnSalvar);
             this.Controls.Add(this.txtNomeCompleto);
             this.Controls.Add(this.label1);
@@ -88,5 +112,7 @@
         private System.Windows.Forms.TextBox txtNomeCompleto;
         private System.Windows.Forms.Button btnSalvar;
         private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.MaskedTextBox mskCPF;
+        private System.Windows.Forms.Label label2;
     }
 }
--- AppCadastros/frmCadClientes.cs
+++ AppCadastros/frmCadClientes.cs
@@ -14,7 +14,95 @@ namespace AppCadastros
     {
         public frmCadClientes()
         {
+            //Serve para criar o visual
+            //da tela
+            //Ou seja ira renderizar o 
+            //formulario
             InitializeComponent();
+
+            //Nescessario para que o sistema
+            //não dispare a validação a todo
+            //momento
+            AutoValidate = AutoValidate.Disable;
+        }
+
+        private void txtNomeCompleto_Validating(object sender, CancelEventArgs e)
+        {
+            //Validar o campo txtNomeCompleto
+            //Se está vazio
+
+            if(string.IsNullOrEmpty(
+                txtNomeCompleto.Text))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(
+                    txtNomeCompleto,
+                    "Preencha o Nome.");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(
+                    txtNomeCompleto,
+                    "");
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if(ValidateChildren(
+                ValidationConstraints.Enabled))
+            {
+                MessageBox.Show(
+                    "Registro Salvo com sucesso!",
+                    "Informação",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Preencha todos os campos corretamente",
+                    "Atenção",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+            }
+        }
+
+        private void mskCPF_Validating(object sender, CancelEventArgs e)
+        {
+            string numero = "";
+
+            //A mascara ira retornar as pontuaçãos
+            //Ex: 123.456.789-00
+            //Porem precisamos apenas do numeros
+            foreach(char c in mskCPF.Text)
+            {
+                if (char.IsDigit(c))
+                    numero += c;
+            }
+
+            //Vaidar se o campo está vazio
+            if(string.IsNullOrEmpty(numero))
+            {
+                e.Cancel= true;
+                errorProvider1.SetError(
+                    mskCPF, "Preencha o CPF.");
+            }
+            //Validar a quantidade de digitos
+            else if (numero.Length != 11)
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(
+                    mskCPF, 
+                    "Preencha o CPF corretamente.");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(
+                    mskCPF, "");
+            }
         }
     }
 }
